09f8f1b148a8c81d6c35f2b3d74a39302f7b0f92
Michael Quigley <michael@quigley.com>
2021-06-22 14:01:20 -0400

    Refactor examples as tests.

---
+++ cf_test.go
@@ -0,0 +1,59 @@
+package cf
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	basic := &struct{
+		StringValue string
+	}{}
+
+	var data = map[string]interface{}{
+		"StringValue": "oh, wow!",
+	}
+
+	err := Load(data, basic)
+	assert.Nil(t, err)
+	assert.Equal(t, "oh, wow!", basic.StringValue)
+}
+
+func TestRenaming(t *testing.T) {
+	renamed := &struct{
+		SomeInt int `cf:"some_int,-required"`
+	}{}
+
+	var data = map[string]interface{}{
+		"some_int": 46,
+	}
+
+	err := Load(data, renamed)
+	assert.Nil(t, err)
+	assert.Equal(t, 46, renamed.SomeInt)
+}
+
+func TestStringArray(t *testing.T) {
+	withArray := &struct{
+		StringArray []string
+	}{}
+
+	var data = map[string]interface{}{
+		"StringArray": []string{"one", "two", "three"},
+	}
+
+	err := Load(data, withArray)
+	assert.Nil(t, err)
+	assert.EqualValues(t, []string{"one", "two", "three"}, withArray.StringArray)
+}
+
+func TestRequired(t *testing.T) {
+	required := &struct{
+		Required int `cf:"-required"`
+	}{}
+
+	data := make(map[string]interface{})
+
+	err := Load(data, required)
+	assert.NotNil(t, err)
+}
