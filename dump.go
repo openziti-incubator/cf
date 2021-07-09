aed25b893de7caca0ce740862de8ebc633e45c2f
Michael Quigley <michael@quigley.com>
2021-07-09 13:32:26 -0400

    Theres our dumper. (#13)

--- dump.go
+++ dump.go
@@ -5,44 +5,53 @@ import (
 	"reflect"
 )
 
-func Tree(cf interface{}) string {
-	return treeNode(reflect.ValueOf(cf), -1)
+func Tree(cf interface{}, opt *Options) string {
+	return treeNode(reflect.ValueOf(cf), -1, opt)
 }
 
-func treeNode(v reflect.Value, indent int) string {
+func treeNode(v reflect.Value, indent int, opt *Options) string {
 	if v.Kind() == reflect.Ptr {
 		v = v.Elem()
 	}
 	switch v.Kind() {
 	case reflect.Struct:
-		return treeNodeStruct(v, indent + 1)
+		return treeNodeStruct(v, indent + 1, opt)
 	case reflect.Slice:
-		return treeNodeSlice(v, indent + 1)
+		return treeNodeSlice(v, indent + 1, opt)
 	default:
-		return fmt.Sprintf("%v", v.Interface())
+		return treeNodeValue(v)
 	}
 }
 
-func treeNodeStruct(v reflect.Value, indent int) string {
+func treeNodeStruct(v reflect.Value, indent int, opt *Options) string {
+	format := fmt.Sprintf("%%-%ds", maxKeyLength(v, opt))
+	fmt.Println(format)
 	out := "{\n"
 	for i := 0; i < v.NumField(); i++ {
 		if v.Field(i).CanInterface() {
-			fd := parseFieldData(v.Type().Field(i), DefaultOptions())
-			out += nLevels(indent + 1) + fd.name + ": " + treeNode(v.Field(i), indent) + "\n"
+			fd := parseFieldData(v.Type().Field(i), opt)
+			out += nLevels(indent + 1) + fmt.Sprintf(format, fd.name) + " = " + treeNode(v.Field(i), indent, opt) + "\n"
 		}
 	}
 	out += nLevels(indent) + "}"
 	return out
 }
 
-func treeNodeSlice(v reflect.Value, indent int) string {
+func treeNodeSlice(v reflect.Value, indent int, opt *Options) string {
 	out := "[\n"
 	for i := 0; i < v.Len(); i++ {
-		out += nLevels(indent + 1) + treeNode(v.Index(i), indent) + "\n"
+		out += nLevels(indent + 1) + treeNode(v.Index(i), indent, opt) + "\n"
 	}
 	out += nLevels(indent) + "]"
 	return out
+}
 
+func treeNodeValue(v reflect.Value) string {
+	if v.Kind() == reflect.String {
+		return fmt.Sprintf("\"%v\"", v.Interface())
+	} else {
+		return fmt.Sprintf("%v", v.Interface())
+	}
 }
 
 func Dump(indent int, cf interface{}, opt *Options) string {
